5ab01958193ed57f2cc256c61783c63cad40eaa0
Ray <dev@hiray.me>
2024-05-09 09:59:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# champion-video-downloader
+
+A simple champion ability video downloader
+
+## Run
+
+### From source
+
+```shell
+git clone https://github.com/hi-ray/champion-video-downloader
+
+cd champion-video-downloader
+
+yarn ## or npm i (I prefer yarn)
+
+yarn start
+```
+### From provided executables
+
+Coming soon...
+
+## Pull requests
+I am happy to recieve pull requests, I only ask that you follow the linting rules.
+
+## License
+LGPL-3.0-only 
+
+see [LICENSE.md](./LICENSE.md)
+
+
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { colorConsole } from 'tracer';
+import download from 'download';
+import { Command } from 'commander';
+import targz from 'targz';
+
+const logger = colorConsole();
+
+// The base path for our champions.
+const ChampionBasePath =
+    'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/{id}.json';
+
+interface options {
+    output: string | undefined;
+}
+
+async function downloadFiles(options: options) {
+    // Check if the user specified an output dir if not default to "./out".
+    if (!options.output) {
+        logger.warn('The output directory has not been defined. Defaulting to "./out".');
+        options.output = './out';
+    }
+
+    // Get the champion ID's,
+    logger.info("Requesting champion ID's from CommunityDragon");
+    const championsReq = await axios.get(
+        'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json',
+    );
+
+    // Get the list of ID's.
+    const championIds: string[] = championsReq.data.map((data) => data.id);
+
+    // Remove the first one(-1) since that isn't a champion.
+    championIds.shift();
+
+    for (let i = 0; i < championIds.length; i++) {
+        // Replace the placeholder {id} with the champions ID.
+        const champUrl = ChampionBasePath.replace('{id}', championIds[i]);
+
+        const req = await axios.get(champUrl);
+
+        const data = req.data;
+
+        data.spells.forEach(({ abilityVideoPath }) => {
+            logger.info(`Downloading ${abilityVideoPath}!`);
+            // Attempt to download the champion ability video some may not exist.
+            try {
+                download(
+                    `https://d28xe8vt774jo5.cloudfront.net/${abilityVideoPath}`,
+                    `${options.output}/${abilityVideoPath.split('/').slice(0, -1).join('/')}`,
+                );
+            } catch (e) {
+                logger.error(`${abilityVideoPath} could not be downloaded, most likely doesn't exist.`);
+            }
+
+            logger.trace(`Downloaded "${abilityVideoPath}".`);
+        });
+    }
+
+    // Compress the videos into a tarball
+    targz.compress(
+        {
+            src: options.output + '/champion-abilities',
+            dest: options.output + '/champion-abilities.tar.gz',
+        },
+        function (err) {
+            if (err) {
+                logger.error(err);
+            } else {
+                logger.info('Successfully created compressed files!');
+            }
+        },
+    );
+}
+
+const program = new Command()
+    .option('-o, --output <output>', 'Output directory location. Default is ./out')
+    .action(async (options: options) => await downloadFiles(options))
+    .version('1.0.0');
+
+// Parse the CLI
+program.parse();
